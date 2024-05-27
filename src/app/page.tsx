4c2497b355dfcd954b822e9bc1c651a2ff294a88
Filip Jaruška <70625490+filipjaruska@users.noreply.github.com>
2024-05-27 23:09:33 +0200

    scaffold UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,24 @@
 import Link from "next/link";
+import { mock } from "node:test";
+
+const mockData = [
+  "https://utfs.io/f/9bbee1ad-ff97-4fa2-a47c-2ad91b77f690-yn8kuo.jpeg",
+]
+const mockImages = mockData.map((url, index) => ({
+  id: index + 1,
+  url
+}))
 
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+    <main className="">
+      <div className="flex flex- gap-4">
+        {[...mockImages, ...mockImages].map((image) => (
+          <div key={image.id} className="w-1/2 p-4">
+            <img src={image.url} />
+          </div>
+        ))}
+      </div>
       <p>Drawdle in progress</p>
     </main>
   );
