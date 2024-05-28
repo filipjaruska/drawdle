001d97763adf39f4e5943fd79a526333bf9a67fa
Filip Jaruška <70625490+filipjaruska@users.noreply.github.com>
2024-05-28 20:59:39 +0200

    temp fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,6 +10,7 @@ export default async function HomePage() {
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
         {images.map((image) => (
           <div key={image.id} className="p-4 border-dashed border-2 border-sky-700 divide-y divide-blue-200">
+            {/* use next Image for the competition, costs money if used too much, nextConfig needs to allow for mnow */}
             <img src={image.url} className="p-2 w-full h-5/6" />
             <div className="text-xl">{image.name}</div>
           </div>
@@ -25,6 +26,9 @@ export default async function HomePage() {
       </SignedOut>
       <SignedIn>
         <UploadImage h1Name="Welcome to your Image Gallery, put here button to upload images! make into ist own client component and put it in /mygalery" />
+        {/* <div>
+          if {images.length} === getMySubmittedImages + 1 then hide the upload button 
+        </div> */}
         <Images />
       </SignedIn>
     </main>
--- src/server/queries.ts
+++ src/server/queries.ts
@@ -5,7 +5,7 @@ import { db } from "~/server/db";
 export async function getMyImages() {
   const user = auth();
 
-  if (!user.userId) throw new Error("User not authorized");
+  // if (!user.userId) throw new Error("User not authorized");
 
   const images = await db.query.images.findMany({
     where: (model, { eq }) => eq(model.userId, user.userId),
@@ -13,6 +13,19 @@ export async function getMyImages() {
   });
   return images;
 }
+
+export async function getMySubmittedImages() {
+  const user = auth();
+
+  if (!user.userId) throw new Error("User not authorized");
+
+  const images = await db.query.images.findMany({
+    where: (model, { not, isNull, eq }) =>
+      not(isNull(model.submitted)) && eq(model.userId, user.userId),
+  });
+  return images.length;
+}
+
 export async function getImages() {
   const images = await db.query.images.findMany({
     orderBy: (model, { desc }) => desc(model.id),
