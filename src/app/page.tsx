76bf10581f3c98918f7c3feee0d79c23fb8f6489
Filip Jaruška <70625490+filipjaruska@users.noreply.github.com>
2024-05-28 15:11:20 +0200

    fixing something wierd

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,6 @@
 import { SignedIn, SignedOut } from "@clerk/nextjs";
 import { db } from "~/server/db";
-import { UploadButton } from "~/utils/uploadthing";
 import UploadImage from "./_components/uploadImage";
-
 export const dynamic = "force-dynamic";
 
 export default async function HomePage() {
